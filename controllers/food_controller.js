bb51e0b8aa3b68a8587aae8aad9259a454a3aad2
Andrett Duvenhage <andrettiduv@gmail.com>
2021-01-13 14:00:37 -0800

    foodController update

--- controllers/food_controller.js
+++ controllers/food_controller.js
@@ -16,3 +16,31 @@ food.post("/", (req, res) => {
         })
     })
 })
+
+Foods.put("/:id", (req, res) => {
+    Foods.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+      (err, updatedFood) => {
+        if (err) {
+          res.send(err)
+        } else {
+          Food.find({}, (err, foundFoods) => {
+            res.json(foundFoods)
+          })
+        }
+      }
+    )
+  })
+
+  foods.delete("/:id", (req, res) => {
+      Food.findByIdAndRemove(req.params.id, (err, deleteFood) => {
+          Food.find({}, (err, foundFoods) => {
+              res.json(foundFoods)
+          })
+      })
+  })
+
+
+module.exports = foods
